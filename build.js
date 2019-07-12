822ae32398d3403deb7aef9ba0008e9c697f5734
Vojko Drev <vojko.drev@adacta.si>
2019-07-12 09:21:07 +0200

    Success checks.

--- build.js
+++ build.js
@@ -9,7 +9,7 @@ if (!String.prototype.endsWith) {
   };
 }
 
-function logWithoutNewLine(kind, text) {
+function logStdWithoutNewLine(kind, text) {
   if (text.endsWith("\r\n")) {
     text = text.substring(0, text.length - 2);
   } else if (text.endsWith("\n")) {
@@ -23,54 +23,72 @@ let p = spawn('cmd.exe');
 
 p.stderr.on('data', (data) => {
   let dataUtf8 = data.toString("utf-8");
-  logWithoutNewLine("stderr", dataUtf8);
+  logStdWithoutNewLine("stderr", dataUtf8);
 
 });
 
 let state = 0;
 
 let executionPlan = [
-  { expect: process.cwd() + ">", command: "cd C:\\code\\configuration\\platform\n" },
-  { expect: "C:\\code\\Configuration\\Platform>", command: "yarn install\n" },
-  
-  { expect: "C:\\code\\Configuration\\Platform>", command: "cd server\\build\n" },
-  { expect: "C:\\code\\Configuration\\Platform\\Server\\build>", command: "start.cmd\n" },
-  { expect: "PS C:\\code\\Configuration\\Platform\\Server\\build> ", command: "Invoke-psake Build\n" },
-  { expect: "PS C:\\code\\Configuration\\Platform\\Server\\build> ", command: "Invoke-psake Restore-Database-MSSQL\n" },
-  { expect: "PS C:\\code\\Configuration\\Platform\\Server\\build> ", command: "exit\n" },
+  { expect: process.cwd() + ">", command: "cd C:\\code\\configuration" },
 
-  { expect: "C:\\code\\Configuration\\Platform\\Server\\build>", command: "cd ../../../Basic/build\n" },
-  { expect: "C:\\code\\Configuration\\Basic\\build>", command: "start.cmd\n" },
-  { expect: "PS C:\\code\\Configuration\\Basic\\build> ", command: "Invoke-psake Build\n" },
-  { expect: "PS C:\\code\\Configuration\\Basic\\build> ", command: "Invoke-psake Execute-Scripts-MSSQL\n" },
-  { expect: "PS C:\\code\\Configuration\\Basic\\build> ", command: "exit\n" },
-
-  { expect: "C:\\code\\Configuration\\Basic\\build>", command: "cd ../../Configuration\n" },
-  { expect: "C:\\code\\Configuration\\Configuration>", command: "yarn install\n" },
-  { expect: "C:\\code\\Configuration\\Configuration>", command: "yarn run es-setup\n" },
-  { expect: "C:\\code\\Configuration\\Configuration>", command: "yarn run publishAll\n" },
-  { expect: "C:\\code\\Configuration\\Configuration>", command: "yarn run import-test-data\n" },
+  { expect: "C:\\code\\Configuration>", command: "cd platform" },
+  { expect: "C:\\code\\Configuration\\Platform>", command: "yarn install", successCheck: "Done in " },
   
-  { expect: "C:\\code\\Configuration\\Configuration>", command: "cd ../platform/client\n" },
-  { expect: "C:\\code\\Configuration\\Platform\\Client>", command: "npm install\n" },
-  { expect: "C:\\code\\Configuration\\Platform\\Client>", command: "bower install\n" },
-  { expect: "C:\\code\\Configuration\\Platform\\Client>", command: "npm run server\n" },
+  { expect: "C:\\code\\Configuration\\Platform>", command: "cd server\\build" },
+  { expect: "C:\\code\\Configuration\\Platform\\Server\\build>", command: "start.cmd", successCheck: "psake succeeded executing " },
+  { expect: "PS C:\\code\\Configuration\\Platform\\Server\\build> ", command: "Invoke-psake Build", successCheck: "psake succeeded executing psakefile.ps1" },
+  { expect: "PS C:\\code\\Configuration\\Platform\\Server\\build> ", command: "Invoke-psake Restore-Database-MSSQL", successCheck: "psake succeeded executing psakefile.ps1" },
+  { expect: "PS C:\\code\\Configuration\\Platform\\Server\\build> ", command: "exit" },
+
+  { expect: "C:\\code\\Configuration\\Platform\\Server\\build>", command: "cd ../../../" },
+
+  { expect: "C:\\code\\Configuration>", command: "cd Basic/build" },
+  { expect: "C:\\code\\Configuration\\Basic\\build>", command: "start.cmd", successCheck: "psake succeeded executing " },
+  { expect: "PS C:\\code\\Configuration\\Basic\\build> ", command: "Invoke-psake Build", successCheck: "psake succeeded executing psakefile.ps1" },
+  { expect: "PS C:\\code\\Configuration\\Basic\\build> ", command: "Invoke-psake Execute-Scripts-MSSQL", successCheck: "psake succeeded executing psakefile.ps1" },
+  { expect: "PS C:\\code\\Configuration\\Basic\\build> ", command: "exit" },
+
+  { expect: "C:\\code\\Configuration\\Basic\\build>", command: "cd ../../" },
+
+  { expect: "C:\\code\\Configuration>", command: "cd configuration" },
+  { expect: "C:\\code\\Configuration\\Configuration>", command: "yarn install", successCheck: "Done in " },
+  { expect: "C:\\code\\Configuration\\Configuration>", command: "yarn run es-setup", successCheck: "Succeeded: " },
+  { expect: "C:\\code\\Configuration\\Configuration>", command: "yarn run publishAll", successCheck: "Done in " },
+  { expect: "C:\\code\\Configuration\\Configuration>", command: "yarn run import-test-data", successCheck: "Testing data imported successfully." },
+  
+  { expect: "C:\\code\\Configuration\\Configuration>", command: "cd ../platform/client" },
+  { expect: "C:\\code\\Configuration\\Platform\\Client>", command: "npm install" },
+  { expect: "C:\\code\\Configuration\\Platform\\Client>", command: "bower install" },
+  { expect: "C:\\code\\Configuration\\Platform\\Client>", command: "npm run server" },
   
 ];
+
+let buffer = "";
 
 p.stdout.on('data', (data) => {
   
   let dataUtf8 = data.toString("utf-8");
 
-  logWithoutNewLine("stdout", dataUtf8);
+  buffer += dataUtf8;
+
+  logStdWithoutNewLine("stdout", dataUtf8);
 
   let r = executionPlan[state];
 
   if (r && dataUtf8.endsWith(r.expect)) {
-    state++;
 
-    logWithoutNewLine("stdin", r.command);
-    p.stdin.write(r.command);
+    let previous = executionPlan[state - 1];
+    if (previous && previous.successCheck && buffer.indexOf(previous.successCheck) == -1) {
+      console.error("FAILED!");
+      process.exit(1);
+    } 
+
+    state++;
+    buffer = "";
+
+    logStdWithoutNewLine("stdin", r.command);
+    p.stdin.write(r.command + "\n");
   }
 
 });
