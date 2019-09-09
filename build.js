4f8efd7b944488485764b0f6c0e241b5985c89e7
Vojko Drev <vojko.drev@adacta.si>
2019-09-09 13:55:15 +0200

    Mono support.

--- build.js
+++ build.js
@@ -18,16 +18,27 @@ const ROOT_PATH = "C:\\code\\Sava";
 let executionPlan = [
   { expect: process.cwd() + `>`, command: `cd ${ROOT_PATH}` },
 
-  { expect: `${ROOT_PATH}>`, command: `cd platform` },
-  { expect: `${ROOT_PATH}\\Platform>`, command: `yarn install`, successCheck: `Done in ` },
+  { expect: `${ROOT_PATH}>`, command: `cd mono` },
+  // { expect: `${ROOT_PATH}\\Platform>`, command: `yarn install`, successCheck: `Done in ` },
   
-  { expect: `${ROOT_PATH}\\Platform>`, command: `cd server\\build` },
-  { expect: `${ROOT_PATH}\\Platform\\Server\\build>`, command: `start.cmd`, successCheck: `psake succeeded executing ` },
-  { expect: `PS ${ROOT_PATH}\\Platform\\Server\\build> `, command: `Invoke-psake Build`, successCheck: `psake succeeded executing psakefile.ps1` },
-  { expect: `PS ${ROOT_PATH}\\Platform\\Server\\build> `, command: `Invoke-psake Restore-Database-Oracle`, successCheck: `psake succeeded executing psakefile.ps1` },
-  { expect: `PS ${ROOT_PATH}\\Platform\\Server\\build> `, command: `exit` },
+  // { expect: `${ROOT_PATH}\\Platform>`, command: `cd server\\build` },
 
-  { expect: `${ROOT_PATH}\\Platform\\Server\\build>`, command: `cd ../../../` },
+  { expect: `${ROOT_PATH}\\mono>`, command: `powershell` },
+  { expect: `PS ${ROOT_PATH}\\mono> `, command: `.\\build.ps1 -Build`, errorCheck: `Build finished with errors` },
+  { expect: `PS ${ROOT_PATH}\\mono> `, command: `.\\build.ps1 -Restore`, successCheck: `Upgrade successful` },
+
+  { expect: `PS ${ROOT_PATH}\\mono> `, command: `exit` },
+
+  { expect: `${ROOT_PATH}\\mono>`, command: `cd ..` },
+
+  // { expect: `PS ${ROOT_PATH}\\mono> `, command: `.\\build.ps1 -ExecuteScripts` },
+
+  // { expect: `${ROOT_PATH}\\Platform\\Server\\build>`, command: `start.cmd`, successCheck: `psake succeeded executing ` },
+  // { expect: `PS ${ROOT_PATH}\\Platform\\Server\\build> `, command: `Invoke-psake Build`, successCheck: `psake succeeded executing psakefile.ps1` },
+  // { expect: `PS ${ROOT_PATH}\\Platform\\Server\\build> `, command: `Invoke-psake Restore-Database-Oracle`, successCheck: `psake succeeded executing psakefile.ps1` },
+  // { expect: `PS ${ROOT_PATH}\\Platform\\Server\\build> `, command: `exit` },
+
+  // { expect: `${ROOT_PATH}\\Platform\\Server\\build>`, command: `cd ../../../` },
   
   { expect: `${ROOT_PATH}>`, command: `cd implementation/build/` },
   { expect: `${ROOT_PATH}\\implementation\\build>`, command: `start.cmd`, successCheck: `psake succeeded executing ` },  
@@ -42,12 +53,17 @@ let executionPlan = [
 
   { expect: `${ROOT_PATH}\\implementation\\Configuration>`, command: `cd ../..` },
 
-  { expect: `${ROOT_PATH}>`, command: `cd Platform\\Client` },
-  { expect: `${ROOT_PATH}\\Platform\\Client>`, command: `npm install` },
-  { expect: `${ROOT_PATH}\\Platform\\Client>`, command: `bower install` },
-  { expect: `${ROOT_PATH}\\Platform\\Client>`, command: `npm run server` },
+  { expect: `${ROOT_PATH}>`, command: `cd mono\\client` },
+  // { expect: `${ROOT_PATH}\\Platform\\Client>`, command: `npm install` },
+  // { expect: `${ROOT_PATH}\\Platform\\Client>`, command: `bower install` },
+  { expect: `${ROOT_PATH}\\mono\\client>`, command: `npm run server` },
   
 ];
+
+function fail() {
+  process.stderr.write("FAILED!\n");
+  process.exit(1);
+}
 
 let state = 0;
 let buffer = "";
@@ -58,20 +74,26 @@ p.stdout.on('data', (data) => {
 
   process.stdout.write(data);
 
-  let r = executionPlan[state];
+  let current = executionPlan[state];
+  let previous = executionPlan[state - 1];
 
-  if (r && buffer.endsWith(r.expect)) {
+  // console.log("current =", current);
+  // console.log("previous =", previous);
+  // console.log("state =", state);
 
-    let previous = executionPlan[state - 1];
+  if (previous && previous.errorCheck && buffer.indexOf(previous.errorCheck) != -1) {
+    fail();
+  }
+  else if (current && buffer.endsWith(current.expect)) {
+
     if (previous && previous.successCheck && buffer.indexOf(previous.successCheck) == -1) {
-      process.stderr.write("FAILED!\n");
-      process.exit(1);
+      fail();
     } 
 
     state++;
     buffer = "";
 
-    p.stdin.write(r.command + "\n");
+    p.stdin.write(current.command + "\n");
   }
 
 });
