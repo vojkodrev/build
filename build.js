e16e64d0165479db57a9bee20f2a7f83097aabb4
Vojko Drev <vojko.drev@adacta-fintech.com>
2020-08-27 10:26:37 +0200

    Success check can now also be an array

--- build.js
+++ build.js
@@ -43,7 +43,7 @@ let executionPlan = [
   { expect: `${ROOT_PATH}\\implementation>`, command: `yarn run publish-workspace`, successCheck: `Done in `, errorCheck: [`[ERROR]`] },
 
   { expect: `${ROOT_PATH}\\implementation>`, command: `powershell` },
-  { expect: `PS ${ROOT_PATH}\\implementation> `, command: `.\\build.ps1 -ExecuteScripts -EnvironmentTarget si -PostPublish`, successCheck: `Upgrade successful`, errorCheck: ['401 Unauthorized'] },
+  { expect: `PS ${ROOT_PATH}\\implementation> `, command: `.\\build.ps1 -ExecuteScripts -EnvironmentTarget si -PostPublish`, successCheck: ['Upgrade successful', 'No new scripts need to be executed - completing.'], errorCheck: ['401 Unauthorized'] },
   { expect: `PS ${ROOT_PATH}\\implementation> `, command: `exit` },
 
   { expect: `${ROOT_PATH}\\implementation>`, command: `cd ..` },
@@ -104,6 +104,27 @@ function failOnError(item, buffer) {
   }
 }
 
+function taskWasSuccessful(successCheck, buffer) { 
+  if (successCheck) {
+    if (Array.isArray(successCheck)) {
+
+      for (let item of successCheck) {
+        if (taskWasSuccessful(item, buffer)) {
+          return true;
+        }
+      }
+
+      return false;
+
+    } else {
+      return buffer.indexOf(successCheck.toUpperCase()) != -1
+    }
+  }
+
+  return true;
+
+}
+
 replaceInFile(`${ROOT_PATH}/mono/build.ps1`, '/nr:false `', '/nr:true `')
 replaceInFile(`${ROOT_PATH}/mono/build.ps1`, '/verbosity:minimal `', '/verbosity:normal `')
 // replaceInFile(`${ROOT_PATH}/mono/build.ps1`, 'Start-Process cmd -ArgumentList "/C npm run server"', '# Start-Process cmd -ArgumentList "/C npm run server"')
@@ -139,7 +160,7 @@ p.stdout.on('data', (data) => {
 
   if (current && (!current.expect || buffer.endsWith(current.expect.toUpperCase()))) {
 
-    if (previous && previous.successCheck && buffer.indexOf(previous.successCheck.toUpperCase()) == -1) {
+    if (previous && !taskWasSuccessful(previous.successCheck, buffer)) {
       fail();
     } 
 
