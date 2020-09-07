bba3c2eba4b7e5bbee6b744e4e83d824f1f7ff5f
Vojko Drev <vojko.drev@adacta-fintech.com>
2020-09-07 13:53:56 +0200

    Disabled post publish scripts.

--- build.js
+++ build.js
@@ -41,9 +41,9 @@ let executionPlan = [
   { expect: `${ROOT_PATH}\\implementation>`, command: `yarn run validate-workspace -e environment.local.json`, successCheck: `Done in `, errorCheck: [`[ERROR]`] },
   { expect: `${ROOT_PATH}\\implementation>`, command: `yarn run publish-workspace -e environment.local.json`, successCheck: `Done in `, errorCheck: [`[ERROR]`] },
 
-  { expect: `${ROOT_PATH}\\implementation>`, command: `powershell` },
-  { expect: `PS ${ROOT_PATH}\\implementation> `, command: `.\\build.ps1 -ExecuteScripts -EnvironmentTarget si -PostPublish`, successCheck: ['Upgrade successful', 'No new scripts need to be executed - completing.'], errorCheck: ['401 Unauthorized', 'No new scripts need to be executed - completing'] },
-  { expect: `PS ${ROOT_PATH}\\implementation> `, command: `exit` },
+  // { expect: `${ROOT_PATH}\\implementation>`, command: `powershell` },
+  // { expect: `PS ${ROOT_PATH}\\implementation> `, command: `.\\build.ps1 -ExecuteScripts -EnvironmentTarget si -PostPublish`, successCheck: ['Upgrade successful', 'No new scripts need to be executed - completing.'], errorCheck: ['401 Unauthorized', 'No new scripts need to be executed - completing'] },
+  // { expect: `PS ${ROOT_PATH}\\implementation> `, command: `exit` },
 
   { expect: `${ROOT_PATH}\\implementation>`, command: `cd ..` },
 
