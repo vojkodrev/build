91ce3577f06a2de610e2788d4f6beb39a20df44b
Vojko Drev <vojko.drev@adacta-fintech.com>
2021-01-11 12:08:43 +0100

    Fixed missing dot in es docker regex.

--- build.ps1
+++ build.ps1
@@ -143,7 +143,7 @@ try {
     -Command 'docker run -p 9200:9200 -m 4g -e "discovery.type=single-node" --name es elasticsearch:7.9.0' `
     -Retry $true `
     -SuccessCheck ".*?Active license is now \[BASIC\]; Security is disabled.*?" `
-    -ErrorCheck ".*?failure in a Windows system call: The virtual machine or container with the specified identifier is not running*?"
+    -ErrorCheck ".*?failure in a Windows system call: The virtual machine or container with the specified identifier is not running.*?"
 
   Remove-Node-Modules
   
